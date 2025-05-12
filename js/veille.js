cbb41cd674203ad4c2ff01f7fb26207299bfbb88
Vexyuu <killianfievet@gmail.com>
2025-05-12 09:58:03 +0200

    Update project links and enhance layout for better user experience

--- js/veille.js
+++ js/veille.js
@@ -6,12 +6,12 @@ const veilles = [
         description: "La reconnaissance faciale est une technologie qui permet d'identifier ou de vérifier l'identité d'une personne à partir de son visage.",
         date: "2023-10-01",
         image: "../image/veille/reconnaissance_faciale.jpg",
-        lien: "https://www.canva.com/design/DAGY5FbVz5I/yYDslK5qI0pDizJkknhfng/view?utm_content=DAGY5FbVz5I&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hfcb236908f",
+        lien: "https://www.canva.com/design/DAGnNy4kb2I/KtkZHFPrtlqfBOyNEvaWrg/view?utm_content=DAGnNy4kb2I&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=haf7cdf4a0e",
         category: "ia"
     }
 ];
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("veille-list");
 
     veilles.forEach(veille => {
